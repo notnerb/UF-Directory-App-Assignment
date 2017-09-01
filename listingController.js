7ab1f23adb85e832822fb27fc5621d3f9ca19e16
Brenton Lin <notnerb.94@ufl.edu>
2017-09-01 15:26:05 -0400

    Delete Working, Add mostly working

--- listingController.js
+++ listingController.js
@@ -3,16 +3,23 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
     $scope.listings = Listings;
     $scope.detailedInfo = 'Click on a entry name for more info';
     $scope.showInfo = false; 
+    $scope.newItem = "";
+    $scope.newItemCode = "";
 
     /* 
       Implement these functions in the controller to make your application function 
       as described in the assignment spec. 
      */
     $scope.addListing = function() {
+      console.log("in add");
+      if ($scope.newItem !== ""){
 
+        $scope.listings.push({"name" : $scope.newItem, "code" : $scope.newItemCode})
+      }
     };
     $scope.deleteListing = function(index) {
-
+        console.log("in delete");
+        $scope.listings.splice(index, 1);
     };
     $scope.showDetails = function(index) {
       $scope.detailedInfo = $scope.listings[index];
