19e15fa374497bf8567059b891fc862ca03b5ad1
Brenton Lin <notnerb.94@ufl.edu>
2017-09-06 19:05:45 -0400

    Work on queries
    Working on queries

--- queries.js
+++ queries.js
@@ -1,10 +1,16 @@
 /* Fill out these functions using Mongoose queries*/
 
+var Listing = require('./ListingSchema.js');
+
 var findLibraryWest = function() {
   /* 
     Find the document that contains data corresponding to Library West,
     then log it to the console. 
    */
+   Listing.find({code: "LBW"}, function(err,Listing){
+    if (err) throw err;
+    console.log(Listing);
+   });
 };
 var removeCable = function() {
   /*
@@ -12,6 +18,12 @@ var removeCable = function() {
     on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
     and remove this listing from your database and log the document to the console. 
    */
+   Listing.find({code: "CABL"}, function(err, listing){
+    if (err) throw err;
+    console.log (listing);
+    listing.remove();
+   });
+
 };
 var updatePhelpsLab = function() {
   /*
